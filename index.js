2331d1bc95d80931b315444b65bfe8bf2d6dc3e3
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-14 23:40:35 +0600

    mongodb add

--- index.js
+++ index.js
@@ -2,7 +2,42 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+//middleware
+const cors = require('cors')
+require('dotenv').config()
+app.use(cors())
+app.use(express.json());
 
+
+// userDB
+// f9I7Gwgg8F0ELs0B
+
+//mongoDB connection
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n6fcc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('mongodb with node curd')
+
+
+        const userCollection = client.db("authenticationDB").collection("user-collection");
+
+
+
+    } finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+//server test
 app.get('/', (req, res) => {
     res.send('authentication server')
 })
